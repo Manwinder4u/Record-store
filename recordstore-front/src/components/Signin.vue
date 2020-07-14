06d0aa56817a08362f926de75d7d15560b5e3eb1
Manwinder singh <mani.winder0007@gmail.com>
2020-07-14 12:26:49 +0530

    first commit

---
+++ recordstore-front/src/components/Signin.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="max-w-sm m-auto my-8">
+    <div class="border p-10 border-grey-light shadow rounded">
+      <h3 class="text-2xl mb-6 text-grey-darkest">Sign In</h3>
+      <form @submit.prevent="signin">
+        <div class="text-red" v-if="error">{{ error }}</div>
+
+        <div class="mb-6">
+          <label for="email" class="block text-gray-700 text-sm font-bold mb-2">E-mail Address</label>
+          <input type="email" v-model="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" placeholder="Manwinder@maropost.com">
+        </div>
+        <div class="mb-6">
+          <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password</label>
+          <input type="password" v-model="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" placeholder="Password">
+        </div>
+        <button type="submit" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-green-500 hover:bg-green-700 block w-full py-4 text-white items-center justify-center">Sign In</button>
+
+        <div class="my-4"><router-link to="/signup" class="link-grey">Sign up</router-link></div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  created () {
+    this.checkSignedIn()
+  },
+  updated () {
+    this.checkSignedIn()
+  },
+  methods: {
+    signin () {
+      this.$http.plain.post('/signin', { email: this.email, password: this.password })
+        .then(response => this.signinSuccessful(response))
+        .catch(error => this.signinFailed(error))
+    },
+    signinSuccessful (response) {
+      if (!response.data.csrf) {
+        this.signinFailed(response)
+        return
+      }
+      localStorage.csrf = response.data.csrf
+      localStorage.signedIn = true
+      this.error = ''
+      this.$router.replace('/records')
+    },
+    signinFailed (error) {
+      this.error = (error.response && error.response.data && error.response.data.error) || ''
+      delete localStorage.csrf
+      delete localStorage.signedIn
+    },
+    checkSignedIn () {
+      if (localStorage.signedIn) {
+        this.$router.replace('/records')
+      }
+    }
+  }
+}
+</script>
